fd8a3f4fec3ebaf9cec9d9b2dda2ee188ded8555
SteveHosko <shosking@vigilant.it>
2016-04-09 11:12:45 +1000

    First Start Point

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# WAPStaterKit
+# WAPStarterKit
---
+++ WAPStarterKit/WAPStarterKit/ConnectPoSH5.ps1
@@ -0,0 +1,19 @@
+﻿#
+# Script.ps1
+#
+Class WapConStrings
+{
+	[string]$AdminAPI
+	[string]$AdminAuth
+	[string]$AdminURI
+	[string]$TenantAPI
+	[string]$TenantAuth
+	[string]$TenantURI
+	[string]$AdmimClientRealm
+	WapConStrings([string]$ComputerName)
+	{
+
+	}
+}
+
+$wap=[WapConStrings]::new()
